0ee25c30e45bbe423931363787fc2b4eaa3a6098
Shashwat Singh <shashwat.singh@pal.tech>
2024-07-25 18:28:20 +0530

    Changes till now

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  res.send('respond with a one resource');
 });
 
-router.get('/cool',(req,res,next)=>res.send(`You're so cool`));
+router.get('/cool', (req, res, next) => res.send(`You're so coosfdfl`));
 module.exports = router;
